b9249b5f2c2471dfa95433598f2ad34962e0e1eb
Steven Kellner <steven.kellner@web.de>
2022-06-11 21:35:30 +0200

    Initial Commit

---
+++ strafen-project-ios/strafen_project_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  strafen_project_iosApp.swift
+//  strafen-project-ios
+//
+//  Created by Steven on 11.06.22.
+//
+
+import SwiftUI
+
+@main
+struct strafen_project_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
